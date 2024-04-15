55acc3f302f488fcf3da61d5b043d5e0a066e667
QPM <admin@QuantumPinnacleMatrix.tech>
2024-04-15 21:13:37 +0300

    fixing the internt issue again on the client internt

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
-import { Routes, BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import LandingPage from "./pages/landing";
-import CategoryDetailPage from "./pages/category-detail";
 import Category from "./pages/category";
+import FreshJuice from "./pages/fresh-jucies";
+import HotDrink from "./pages/hotdrinks";
+import Juice from "./pages/juice";
+import Fakfakhena from "./pages/milkshake1";
+import Milkshakes from "./pages/milkshake2";
+import Sweet from "./pages/sweet";
+
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/categories" element={<Category />} />
-        <Route path="/categories/:category" element={<CategoryDetailPage />} />
+        <Route path="/categories/fresh-juice" element={<FreshJuice />} />
+        <Route path="/categories/hotdrinks" element={<HotDrink />} />
+        <Route path="/categories/juice" element={<Juice />} />
+        <Route path="/categories/milkshake1" element={<Fakfakhena />} />
+        <Route path="/categories/milkshake2" element={<Milkshakes />} />
+        <Route path="/categories/sweet" element={<Sweet />} />
+        {/* <Route path="/categories/:category" element={<CategoryDetailPage />} /> */}
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
---
+++ src/pages/fresh-jucies.tsx
@@ -0,0 +1,65 @@
+import Footer from "@/components/footer";
+import MenuHandlers from "@/components/menu-handler";
+import { FreshJuices } from "@/constants/fresh-juices";
+import { motion } from "framer-motion";
+import { useParams } from "react-router-dom";
+const FreshJuice = () => {
+  const params = useParams();
+
+  const variants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 1 } },
+    exit: { opacity: 0, transition: { duration: 0.5 } },
+  };
+
+  return (
+    <motion.main
+      className="font-amiri z-0 flex flex-col items-center justify-center min-h-screen px-4 py-12 bg-green-200 relative"
+      dir="rtl"
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+      variants={variants}
+    >
+      <motion.h1
+        className="font-bold text-4xl text-center mb-8"
+        variants={variants}
+      >
+        مشروبات بنكة طبيعية
+      </motion.h1>
+      <motion.section
+        className="flex flex-col items-center w-full"
+        variants={variants}
+      >
+        {FreshJuices.map((item, index) => (
+          <motion.div
+            key={index}
+            className="bg-white shadow-lg rounded-lg p-6 mb-6 w-full md:w-3/4 lg:w-1/2 text-right"
+            whileHover={{ scale: 1.02 }}
+            whileTap={{ scale: 0.98 }}
+            variants={variants}
+          >
+            <img
+              src={item.image}
+              alt={item.title}
+              className="w-full h-64 object-fit rounded-lg mb-4"
+            />
+            <h2 className="font-bold text-2xl mb-2">{item.title}</h2>
+            <p className=" font-bold text-xl">{item.price} ₪</p>
+          </motion.div>
+        ))}
+      </motion.section>
+      <motion.div
+        className="flex flex-row space-x-4 justify-start items-center w-full"
+        variants={variants}
+      >
+        <MenuHandlers pathname={params.category!} />
+      </motion.div>
+      <footer>
+        <Footer />
+      </footer>
+    </motion.main>
+  );
+};
+
+export default FreshJuice;
