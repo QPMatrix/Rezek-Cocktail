7e4a8649a2472880d139d270972b1ca0b4e98908
QPM <admin@QuantumPinnacleMatrix.tech>
2024-04-15 21:25:00 +0300

    last try

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,36 @@
+import { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Loading from "./components/loading";
 
-import LandingPage from "./pages/landing";
-import Category from "./pages/category";
-import FreshJuice from "./pages/fresh-jucies";
-import HotDrink from "./pages/hotdrinks";
-import Juice from "./pages/juice";
-import Fakfakhena from "./pages/milkshake1";
-import Milkshakes from "./pages/milkshake2";
-import Sweet from "./pages/sweet";
+const LandingPage = lazy(() => import("./pages/landing"));
+// const CategoryDetailPage = lazy(() => import("./pages/category-detail"));
+const Category = lazy(() => import("./pages/category"));
+const FreshJuice = lazy(() => import("./pages/fresh-jucies"));
+const HotDrink = lazy(() => import("./pages/hotdrinks"));
+const Juice = lazy(() => import("./pages/juice"));
+const Fakfakhena = lazy(() => import("./pages/milkshake1"));
+const Milkshakes = lazy(() => import("./pages/milkshake2"));
+const Sweet = lazy(() => import("./pages/sweet"));
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/categories" element={<Category />} />
-        <Route path="/categories/fresh-juice" element={<FreshJuice />} />
-        <Route path="/categories/hotdrinks" element={<HotDrink />} />
-        <Route path="/categories/juice" element={<Juice />} />
-        <Route path="/categories/milkshake1" element={<Fakfakhena />} />
-        <Route path="/categories/milkshake2" element={<Milkshakes />} />
-        <Route path="/categories/sweet" element={<Sweet />} />
-        {/* <Route path="/categories/:category" element={<CategoryDetailPage />} /> */}
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/categories" element={<Category />} />
+          <Route path="/categories/fresh-juice" element={<FreshJuice />} />
+          <Route path="/categories/hotdrinks" element={<HotDrink />} />
+          <Route path="/categories/juice" element={<Juice />} />
+          <Route path="/categories/milkshake1" element={<Fakfakhena />} />
+          <Route path="/categories/milkshake2" element={<Milkshakes />} />
+          <Route path="/categories/sweet" element={<Sweet />} />
+          {/* <Route
+            path="/categories/:category"
+            element={<CategoryDetailPage />}
+          /> */}
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
